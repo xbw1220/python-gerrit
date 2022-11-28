607f27b7852c7d56038d8246420610857245db1c
xbw1220 <30739769+xbw1220@users.noreply.github.com>
2022-11-28 10:49:01 +0800

    init

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import config, excel, time
 start_time = '2022-07-01'
 end_time = '2022-11-25'
 excel_name = 'Gerrit统计2022-11-25.xlsx'
-datas = excel.get_total_data(start_time, end_time, config.user_dict)
-excel.create_total_excel_file(datas, excel_name)
-time.sleep(3)
-excel.get_user_data(start_time, end_time, config.user_dict, excel_name)
+
+if __name__ == "__main_":
+    datas = excel.get_total_data(start_time, end_time, config.user_dict)
+    excel.create_total_excel_file(datas, excel_name)
+    time.sleep(3)
+    excel.get_user_data(start_time, end_time, config.user_dict, excel_name)
