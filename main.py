f7193d607ab87ed8418170576b9044171ef2591f
xbw1220 <30739769+xbw1220@users.noreply.github.com>
2022-12-02 13:36:36 +0800

    init

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ start_time = '2022-07-01'
 end_time = '2022-11-25'
 excel_name = 'Gerrit统计-{0}.xlsx'.format(end_time)
 
-if __name__ == "__main_":
+if __name__ == '__main__':
     datas = excel.get_total_data(start_time, end_time, config.user_dict)
     excel.create_total_excel_file(datas, excel_name)
     time.sleep(3)
